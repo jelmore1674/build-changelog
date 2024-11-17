063479d67a02b5569b6999238127129d1e78023b
Justin Elmore <jelmore1674@pm.me>
2024-11-17 15:20:26 -0600

    add support for yaml config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,54 @@
 #!/usr/bin/env node
 
-import { readdirSync, readFileSync, writeFileSync } from "node:fs";
+import { existsSync, readdirSync, readFileSync, writeFileSync } from "node:fs";
 import path from "node:path";
 import YAML from "yaml";
 import { generateChangelog, type Keywords, type Version, type YamlChanges } from "./templates";
 import { isYamlFile } from "./utils/isYamlFile";
 
-const changelogDir = process.argv[2];
+type Config = {
+  /** the directory our changlog files will be in */
+  dir: string;
+  /** Custom list of keywords. */
+  keywords: string[];
+  /** Custom list of flags. */
+  flags: string[];
+  /** The separator to change how we do the date of a version */
+  version_date_separator: string;
+};
 
-const dir = path.join(process.cwd(), changelogDir);
+const initialKeywords: Keywords[] = ["added", "changed", "fixed", "deprecated", "removed", "security"];
+const initialFlags = ["breaking"];
+
+const initialConfig: Config = {
+  dir: "changelog",
+  keywords: initialKeywords,
+  flags: initialFlags,
+  version_date_separator: "_",
+};
+
+let rawConfig = "";
+let config = initialConfig;
+
+// Look for the `bcl.yml` config file.
+const configPath = path.join(process.cwd(), "bcl.yml");
+
+// Use the initial config if we do not have a config file.
+if (!existsSync(configPath)) {
+} else {
+  rawConfig = readFileSync(configPath, { encoding: "utf8" });
+  config = { ...initialConfig, ...YAML.parse(rawConfig) };
+}
+
+const dir = path.join(process.cwd(), config.dir);
 const files = readdirSync(dir, { recursive: true });
 
 const versions = files.reduce((acc: Version[], dir) => {
   if (typeof dir === "string") {
     if (isYamlFile(dir)) {
-      const d = path.join(process.cwd(), `${changelogDir}/${dir}`);
-      const [release, releaseDate] = path.basename(path.dirname(d)).split("_");
+      const d = path.join(process.cwd(), `${config.dir}/${dir}`);
+
+      const [release, releaseDate] = path.basename(path.dirname(d)).split(config.version_date_separator);
 
       const exists = acc.find(({ version }) => version === release);
 
