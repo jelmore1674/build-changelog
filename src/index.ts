f2d435463a93a4a347f5d8e7997b76a92479a207
Justin Elmore <jelmore1674@pm.me>
2024-11-17 22:16:47 -0600

    update pipeline to have cache

--- src/index.ts
+++ src/index.ts
@@ -57,26 +57,28 @@ const versions = files.reduce((acc: Version[], dir) => {
       const changes = readFileSync(d, { encoding: "utf8" });
       const yaml: YamlChanges = YAML.parse(changes);
 
-      const yamlKeys = Object.keys(yaml) as Keywords[];
+      if (yaml) {
+        const yamlKeys = Object.keys(yaml) as Keywords[];
 
-      for (const key of yamlKeys) {
-        if (!currentVersion[key]) {
-          currentVersion[key] = [];
-        }
+        for (const key of yamlKeys) {
+          if (!currentVersion[key]) {
+            currentVersion[key] = [];
+          }
 
-        if (yaml[key]) {
-          // @ts-ignore - breaking could exist.
-          if (yaml[key]?.breaking) {
-            // @ts-ignore - breaking exists.
-            currentVersion[key].push(...yaml[key].breaking.map((str: string) => `[Breaking 🧨] - ${str}`));
-          } else {
-            currentVersion[key].push(...yaml[key] as string[]);
+          if (yaml[key]) {
+            // @ts-ignore - breaking could exist.
+            if (yaml[key]?.breaking) {
+              // @ts-ignore - breaking exists.
+              currentVersion[key].push(...yaml[key].breaking.map((str: string) => `[Breaking 🧨] - ${str}`));
+            } else {
+              currentVersion[key].push(...yaml[key] as string[]);
+            }
           }
         }
-      }
 
-      if (!exists) {
-        acc.push(currentVersion);
+        if (!exists) {
+          acc.push(currentVersion);
+        }
       }
     }
   }
