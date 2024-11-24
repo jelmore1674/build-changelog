f3191bd39ef0ae86702c1c43c7a27d4065625a23
Justin Elmore <jelmore1674@pm.me>
2024-11-23 23:58:22 -0600

    bump version

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ const program = new Command();
 program
   .name("build-changelog")
   .description("cli tool to generate changelogs")
-  .version("0.2.4");
+  .version("0.2.5");
 
 for (const command of commands) {
   // This is to keep the changelog command alive. for now. Will be deprecated in the future.
