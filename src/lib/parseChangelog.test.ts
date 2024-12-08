d4d4905bfc83f5a61d48108d9fed2f1e48185e50
Justin Elmore <jelmore1674@pm.me>
2024-12-08 13:51:22 -0600

    add parseChangelog

---
+++ src/lib/parseChangelog.test.ts
@@ -0,0 +1,154 @@
+import { removeSync } from "fs-extra";
+import * as fs from "node:fs";
+import path from "node:path";
+import { afterEach, describe, expect, test, vi } from "vitest";
+import { configPath } from "./config";
+import { parseChangelog } from "./parseChangelog";
+
+const dir = process.env.NODE_ENV === "test" ? process.cwd() : __dirname;
+
+const TEST_DIR = path.join(dir, "../../test");
+
+function teardown() {
+  // Remove test dir that is generated.
+  if (fs.existsSync(TEST_DIR)) {
+    removeSync(TEST_DIR);
+  }
+
+  // Remove test config that is generated
+  if (fs.existsSync(configPath)) {
+    removeSync(configPath);
+  }
+}
+
+describe("convertChangelogToObject", () => {
+  vi.mock(import("node:fs"), async (importOriginal) => {
+    const og = await importOriginal();
+    return ({
+      readFileSync: vi.fn().mockImplementation(og.readFileSync),
+      writeFileSync: vi.fn().mockImplementation(og.writeFileSync),
+      existsSync: vi.fn().mockImplementation(og.existsSync),
+      mkdirSync: vi.fn().mockImplementation(og.mkdirSync),
+    });
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    teardown();
+  });
+
+  test("reads changelog", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n## [1.0.0] - 2024-12-07\n\n### Added\n\n- This cool change`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{ version: "1.0.0", release_date: "2024-12-07", added: ["This cool change"] }]);
+  });
+
+  test("can handle multiple changes", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n## [1.0.0] - 2024-12-07\n\n### Added\n\n- This cool change\n- Another change\n- And another one`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "1.0.0",
+      release_date: "2024-12-07",
+      added: ["This cool change", "Another change", "And another one"],
+    }]);
+  });
+
+  test("can get the version without brackets.", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n## 1.0.0 - 2024-12-07\n\n### Added\n\n- This cool change\n- Another change\n- And another one`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "1.0.0",
+      release_date: "2024-12-07",
+      added: ["This cool change", "Another change", "And another one"],
+    }]);
+  });
+
+  test("if version is not in valid semver it will fallback to unreleased", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n##  upcoming \n\n### Added\n\n- This cool change\n- Another change\n- And another one`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "Unreleased",
+      release_date: "TBD",
+      added: ["This cool change", "Another change", "And another one"],
+    }]);
+  });
+
+  test("should omit empty changes.", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# changelog
+
+## [1.2.4] - 2017-03-15
+
+Here we would have the update steps for 1.2.4 for people to follow.
+
+### Added
+
+### Changed
+
+- [PROJECTNAME-ZZZZ](http://tickets.projectname.com/browse/PROJECTNAME-ZZZZ)
+  PATCH Drupal.org is now used for composer.
+
+### Fixed
+
+- [PROJECTNAME-TTTT](http://tickets.projectname.com/browse/PROJECTNAME-TTTT)
+  PATCH Add logic to runsheet teaser delete to delete corresponding
+  schedule cards.`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "1.2.4",
+      release_date: "2017-03-15",
+      added: [],
+      changed: [
+        "[PROJECTNAME-ZZZZ](http://tickets.projectname.com/browse/PROJECTNAME-ZZZZ) PATCH Drupal.org is now used for composer.",
+      ],
+      fixed: [
+        "[PROJECTNAME-TTTT](http://tickets.projectname.com/browse/PROJECTNAME-TTTT) PATCH Add logic to runsheet teaser delete to delete corresponding schedule cards.",
+      ],
+    }]);
+  });
+
+  test("all keywords are valid", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n## [1.0.0] - 2024-12-07\n\n### Added\n\n- This cool change\n\n### Fixed\n\n- this is a fix.\n\n### Changed\n\n- this is a change\n\n### Removed\n\n- this remove a feature\n\n### Deprecated\n\n- this will be deprecated\n\n### Security\n\n- security update`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "1.0.0",
+      release_date: "2024-12-07",
+      added: ["This cool change"],
+      fixed: ["this is a fix."],
+      changed: ["this is a change"],
+      removed: ["this remove a feature"],
+      deprecated: ["this will be deprecated"],
+      security: ["security update"],
+    }]);
+  });
+
+  test("all keywords are valid", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValueOnce(
+      `# Changelog\n\n## [1.0.0] - 2024-12-07\n\n### Added\n\n- [Breaking 🧨] - this cool change\n`,
+    );
+    const cl = parseChangelog("CHANGELOG.md");
+    expect(cl).toEqual([{
+      version: "1.0.0",
+      release_date: "2024-12-07",
+      added: ["[Breaking 🧨] - this cool change"],
+    }]);
+  });
+});
---
+++ src/lib/parseChangelog.ts
@@ -0,0 +1,68 @@
+import { existsSync, readFileSync } from "node:fs";
+import path from "node:path";
+import { Keywords, Version } from "./mustache";
+
+// semver versioning
+const versionRegex = /\d\.\d\.\d/gism;
+// release version section.
+const versionSectionRegex = /(?=^#{2} .*?$)/gism;
+// date: looks for either 2024-12-06 or 12-06-2024
+const dateRegex = /\d{1,4}(-|\/)\d{1,2}(-|\/)\d{1,4}/gism;
+// the changes section.
+const changeSectionRegex = /(?=^#{3} .*?$)/gism;
+// keywords
+const keywordRegex = /(?:fixed|removed|changed|added|security|deprecated)/gism;
+const changesRegex = /^#{3} (?:fixed|removed|changed|added|security|deprecated)/gism;
+
+/**
+ * parse an existing changelog file and convert to an object.
+ */
+function parseChangelog(changelogPath: string) {
+  if (existsSync(changelogPath)) {
+    const cl = readFileSync(changelogPath, { encoding: "utf8" });
+
+    const changelog = cl.split(versionSectionRegex).map(section => {
+      if (section.toLowerCase().includes("# changelog")) {
+        return null;
+      }
+
+      // Pull the version and release_date from the section.
+      const [version] = section.match(versionRegex) || ["Unreleased"];
+      const [release_date] = section.match(dateRegex) || ["TBD"];
+      // TODO: Add a way to add upgrade steps to parsing. ? Maybe. Unsure how to parse that.
+
+      // Intialize the release version..
+      const release: Version = {
+        version,
+        release_date,
+      };
+
+      section.split(changeSectionRegex).map(changeKeyword => {
+        if (changeKeyword.match(versionSectionRegex)) {
+          return null;
+        }
+
+        const [keyword] = changeKeyword.match(keywordRegex) as string[];
+
+        if (keyword) {
+          release[keyword.toLowerCase() as Keywords] = changeKeyword.replace(
+            changesRegex,
+            "",
+          ).trim().split("\n- ")
+            .map((change: string) => change.replace(/^- /g, "").replace(/\n /g, ""))
+            .filter(i => i);
+        }
+      }).filter(changes => {
+        return changes;
+      });
+
+      return release;
+    }).filter(release => release) as Version[];
+
+    return changelog;
+  }
+
+  return [];
+}
+
+export { parseChangelog };
