374aa8474b971c51f234088e7f358655ab3570a2
Justin Elmore <jelmore1674@pm.me>
2024-12-28 00:59:33 -0600

    bump version

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,12 @@ import { generateCommand } from "./lib/generate";
 import { initCommand } from "./lib/init";
 import { notesCommand } from "./lib/releaseNotes";
 
-// const commands = [
-//  // TODO: `add` command to add a template changelog based on the git branch. (Maybe auto generate?).
-//  {
-//    name: "ac",
-//    description: "add a change",
-//    action: () => {
-//      exec("git rev-parse --abbrev-ref HEAD", (err, stdout, _stderr) => {
-//        if (err) {
-//          // handle error
-//        }
-//        const branch = stdout.trim();
-//        console.log(`Current branch: ${branch}`);
-//      });
-//    },
-//  },
-// ];
-
 const program = new Command();
 
 program
   .name("build-changelog")
   .description("cli tool to generate changelogs")
-  .version("0.5.5");
+  .version("1.0.0");
 
 program
   .command("init")
