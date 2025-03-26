1ee2b9c955830cedfc2fa15b6e14e9fd2ba310b0
Justin Elmore <jelmore1674@pm.me>
2025-03-25 22:15:00 -0500

    add context log

--- src/generate.ts
+++ src/generate.ts
@@ -1,4 +1,4 @@
-import { getOctokit } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import { generateCommand } from "./lib/generate";
 
 const GITHUB_ACTOR = process.env.GITHUB_ACTOR;
@@ -8,6 +8,8 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
  * Get the name of the author from github.
  */
 async function getAuthorName() {
+  // Log this to see what we get on a merge.
+  console.log({ context });
   const user = await getOctokit(GITHUB_TOKEN as string).rest.users.getByUsername({
     username: GITHUB_ACTOR as string,
   });
