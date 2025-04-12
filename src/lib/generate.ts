4c2e367daa0b8a91fe25358d184eba092faa2839
Justin Elmore <jelmore1674@pm.me>
2025-04-11 23:42:09 -0500

    enforcer test

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -143,7 +143,7 @@ function generateChange(
   let renderedChange = change;
 
   // Generate the links for the change.
-  if (references.length && GITHUB_REPOSITORY) {
+  if (GITHUB_REPOSITORY) {
     renderedChange = `${change} (${
       generateReferences([
         ...((config?.reference_pull_requests && prNumber)
