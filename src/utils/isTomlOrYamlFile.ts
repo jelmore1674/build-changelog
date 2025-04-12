05d31046df78788494132371d1f6068c8ef5f21f
Justin Elmore <jelmore1674@pm.me>
2025-04-11 21:46:04 -0500

    move isTomlOrYaml to utils

---
+++ src/utils/isTomlOrYamlFile.ts
@@ -0,0 +1,13 @@
+import { isTomlFile } from "./isTomlFile";
+import { isYamlFile } from "./isYamlFile";
+
+/**
+ * Utility function that determines if the file is a `TOML` or `YAML` file.
+ *
+ * @param file - the file name you are checking.
+ */
+function isTomlOrYamlFile(file: string) {
+  return isTomlFile(file) || isYamlFile(file);
+}
+
+export {isTomlOrYamlFile}
