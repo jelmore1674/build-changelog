a3cf41e2b2c7d0aa963609c868cf614f417d5f0f
Justin Elmore <jelmore1674@pm.me>
2025-04-19 15:26:31 -0500

    udpate enforcer

--- src/actions/enforcer.ts
+++ src/actions/enforcer.ts
@@ -1,24 +1,10 @@
 import { debug, getInput, setFailed } from "@actions/core";
-import { exec, getExecOutput } from "@actions/exec";
+import { getExecOutput } from "@actions/exec";
 import { context } from "@actions/github";
-import { readdirSync, rmSync, writeFileSync } from "node:fs";
 import { exit } from "node:process";
-import YAML from "yaml";
-import { generateCommand, parseChanges } from "../lib/generate";
-import { isYamlFile } from "../utils/isYamlFile";
-import { log } from "../utils/log";
-import { commitWithApi } from "./utils/commitWithApi";
+import { generateCommand } from "../lib/generate";
+import { addChangelogDependabot } from "./utils/addChangelogDependabot";
 import { stringToBoolean } from "./utils/stringToBoolean";
-
-/**
- * Regex used to get the changes from the pr body.
- */
-const dependabotRegex = /^(?:(?:U|u)pdate|(?:B|b)ump)s? (.*?) (?:requirement )?from (.*) to (.*)/gm;
-
-interface DependabotChangeFile {
-  author: string;
-  security: string[];
-}
 
 /**
  * Run the generate command and check the git diff to see if there are changes
@@ -36,48 +22,7 @@ async function enforceChangelogAction() {
     enableDependabot && dependabotLabels.some(label => set.has(label))
     && pullRequest?.body && pullRequest?.user.login === "dependabot[bot]"
   ) {
-    const matches = pullRequest.body.match(dependabotRegex);
-
-    if (matches && matches.length > 0) {
-      debug(`matches found\n${matches}`);
-
-      const dependabotUpdates = {
-        author: "dependabot",
-        security: matches,
-      };
-
-      const changelogFiles = readdirSync("./changelog", { recursive: true, encoding: "utf8" });
-
-      debug(`Files found:\n${changelogFiles}`);
-
-      for (const file of changelogFiles) {
-        if (isYamlFile(file)) {
-          const filePath = `./changelog/${file}`;
-          const parsedFile = parseChanges<DependabotChangeFile>(filePath);
-
-          debug(`File: ${file}\n\n${JSON.stringify(parsedFile, null, 2)}`);
-
-          if (parsedFile.author === "dependabot") {
-            if (matches.every(change => parsedFile.security.includes(change))) {
-              log("No new changes. Stopping action.");
-              exit(0);
-            }
-
-            // Remove the file and continue.
-            log("Removing the previous change file.");
-            rmSync(filePath, { force: true });
-          }
-        }
-      }
-
-      const ymlFile = YAML.stringify(dependabotUpdates);
-      writeFileSync(`./changelog/${context.sha}-${context.runId}.yml`, ymlFile, {
-        encoding: "utf8",
-      });
-
-      await exec("git", ["add", "."]);
-      await commitWithApi("Add changelog file for dependabot.");
-    }
+    await addChangelogDependabot(pullRequest.body);
   }
 
   if (skipLabels.some(label => set.has(label))) {
