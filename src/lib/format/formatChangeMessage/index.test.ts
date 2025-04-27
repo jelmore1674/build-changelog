4d061df13b08d6918c8c1545cd2c993d602a6533
Justin Elmore <jelmore1674@pm.me>
2025-04-27 00:11:05 -0500

    add formatChange

---
+++ src/lib/format/formatChangeMessage/index.test.ts
@@ -0,0 +1,143 @@
+import { describe, expect, test } from "vitest";
+import { formatChangeMessage } from "./";
+
+const author = "justin";
+const message = "This is a change.";
+const sha = "3c20f6dc679fbece838d9828bd8391d5d371ac63";
+
+describe("formatChangeMessage", () => {
+  test("Can render without options", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+    }, {});
+
+    expect(result).toBe("This is a change.");
+  });
+
+  test("Can render with sha.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+    }, {
+      reference_sha: true,
+    });
+
+    expect(result).toBe(
+      "This is a change. [`3c20f6d`](https://github.com/jelmore1674/build-changelog/commit/3c20f6dc679fbece838d9828bd8391d5d371ac63)",
+    );
+  });
+
+  test("Can render just references.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [{ type: "issue", number: 1 }],
+    }, {});
+
+    expect(result).toBe(
+      "This is a change. | [#1](https://github.com/jelmore1674/build-changelog/issues/1)",
+    );
+  });
+
+  test("Can render reference from pr without other references.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+      prNumber: 2,
+    }, { reference_pull_requests: true });
+
+    expect(result).toBe(
+      "This is a change. | [#2](https://github.com/jelmore1674/build-changelog/pull/2)",
+    );
+  });
+
+  test("Can render reference from pr.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [{ type: "issue", number: 1 }],
+      prNumber: 2,
+    }, { reference_pull_requests: true });
+
+    expect(result).toBe(
+      "This is a change. | [#1](https://github.com/jelmore1674/build-changelog/issues/1), [#2](https://github.com/jelmore1674/build-changelog/pull/2)",
+    );
+  });
+
+  test("Can render message with just author.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+    }, { show_author: true });
+
+    expect(result).toBe(
+      "This is a change. | [justin](https://github.com/jelmore1674)",
+    );
+  });
+
+  test("Can render hash and author", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+    }, { show_author: true, reference_sha: true });
+
+    expect(result).toBe(
+      "This is a change. [`3c20f6d`](https://github.com/jelmore1674/build-changelog/commit/3c20f6dc679fbece838d9828bd8391d5d371ac63) | [justin](https://github.com/jelmore1674)",
+    );
+  });
+
+  test("Can render hash and author with flag", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+      flag: "breaking",
+    }, { show_author: true, reference_sha: true, flags: { breaking: "[Breaking]" } });
+
+    expect(result).toBe(
+      "[Breaking] - This is a change. [`3c20f6d`](https://github.com/jelmore1674/build-changelog/commit/3c20f6dc679fbece838d9828bd8391d5d371ac63) | [justin](https://github.com/jelmore1674)",
+    );
+  });
+
+  test("Can render message with just flag.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+      flag: "breaking",
+    }, { flags: { breaking: "[Breaking 🧨]" } });
+
+    expect(result).toBe(
+      "[Breaking 🧨] - This is a change.",
+    );
+  });
+
+  test("Can render message with custom flag.", () => {
+    const result = formatChangeMessage({
+      message,
+      author,
+      sha,
+      references: [],
+      flag: "custom",
+    }, { flags: { custom: "[Custom 🎉]" } });
+
+    expect(result).toBe(
+      "[Custom 🎉] - This is a change.",
+    );
+  });
+});
---
+++ src/lib/format/formatChangeMessage/index.ts
@@ -0,0 +1,70 @@
+import type { GenerateConfig, Reference } from "@types";
+import { log } from "@utils/log";
+import { formatAuthor } from "../formatAuthor";
+import { formatCommitSha } from "../formatCommitSha";
+import { formatReferences } from "../formatReferences";
+
+interface ChangeMessage {
+  message: string;
+  references: Reference[];
+  author: string;
+  sha: string;
+  prNumber?: number;
+  flag?: string;
+}
+
+/**
+ * Generate the change entry in the changelog.
+ *
+ * @param change - the change message
+ * @param changelogLinks - the links from the changelog
+ * @param references - any references to create link.
+ * @param config - the configuration
+ * @param author - the author
+ * @param [flag] - optional flag that can be used.
+ * @param [prNumber] - pr number to auto reference.
+ */
+function formatChangeMessage(
+  {
+    message,
+    references,
+    author,
+    sha,
+    prNumber,
+    flag,
+  }: ChangeMessage,
+  config: GenerateConfig,
+) {
+  let renderedChange = message;
+
+  // Generate the links for the change.
+  if (config.reference_sha) {
+    renderedChange = `${renderedChange} ${formatCommitSha(sha)}`;
+  }
+
+  if (references.length || prNumber) {
+    renderedChange = `${renderedChange} | ${
+      formatReferences([
+        ...((config?.reference_pull_requests && prNumber)
+          ? [{ type: "pull_request", number: prNumber }] as Reference[]
+          : []),
+        ...references,
+      ])
+    }`;
+  }
+
+  // Add author to the change.
+  if (config.show_author) {
+    renderedChange = `${renderedChange} | ${formatAuthor(author)}`;
+  }
+
+  if (flag && config.flags?.[flag]) {
+    return `${config.flags?.[flag]} - ${renderedChange}`;
+  }
+
+  log(`Change: ${renderedChange}`);
+
+  return renderedChange;
+}
+
+export { formatChangeMessage };
