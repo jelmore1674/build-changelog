0695668505c062f6306ff298277057e3a54fed07
Justin Elmore <jelmore1674@pm.me>
2025-04-27 15:01:21 -0500

    udpate release notes and tests to use memfs

--- src/lib/releaseNotes/index.test.ts
+++ src/lib/releaseNotes/index.test.ts
@@ -1,38 +1,42 @@
-import TOML, { JsonMap } from "@iarna/toml";
-import { removeSync } from "fs-extra";
-import { existsSync, mkdirSync, writeFileSync } from "node:fs";
-import { expect, test, vitest } from "vitest";
-import { changelogDir, configPath } from "../config";
+import { fs, vol } from "memfs";
+import { afterEach, describe, expect, test, vi, vitest } from "vitest";
 import * as releaseNotes from "./";
 
-const TEST_DIR = "/src/test";
+vi.mock("node:fs", async () => {
+  const memfs: { fs: typeof fs } = await vi.importActual("memfs");
 
-function teardown() {
-  // Remove test dir that is generated.
-  if (existsSync(TEST_DIR)) {
-    removeSync(TEST_DIR);
-  }
-
-  // Remove test config that is generated
-  if (existsSync(configPath)) {
-    removeSync(configPath);
-  }
-}
-
-test("should run successful", async () => {
-  const notesCommand = vitest.spyOn(releaseNotes, "notesCommand");
-  releaseNotes.notesCommand();
-
-  expect(notesCommand).toHaveBeenCalledOnce();
-  expect(notesCommand).toReturn();
+  return memfs.fs;
 });
 
-test("should read match version from archive.", async () => {
-  const notesCommand = vitest.spyOn(releaseNotes, "notesCommand");
-  releaseNotes.notesCommand("1.0.0");
+describe("releaseNotes", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+    vol.reset();
+  });
 
-  expect(notesCommand).toHaveBeenCalledOnce();
-  expect(notesCommand).toReturn();
+  test("should run successful", async () => {
+    vol.fromJSON({
+      "./CHANGELOG.md": `# Changelog
 
-  teardown();
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [v0.1.0] - 2025-01-01
+
+### Fixed
+
+- This test issue [\`abcdef3\`](https://github.com/jelmore1674/build-changelog/commit/abcdef3149d) | [bcl-bot](https://github.com/jelmore1674)\n\n`,
+    });
+
+    const notesCommand = vitest.spyOn(releaseNotes, "notesCommand");
+    const result = releaseNotes.notesCommand();
+
+    expect(notesCommand).toHaveBeenCalledOnce();
+    expect(notesCommand).toReturn();
+    expect(result).toBe(`# What's Changed\n\n## Fixed
+
+- This test issue [\`abcdef3\`](https://github.com/jelmore1674/build-changelog/commit/abcdef3149d) | [bcl-bot](https://github.com/jelmore1674)\n\n`);
+  });
 });
