d969ca2c1103de89fb001af50b887d80883258a5
Justin Elmore <jelmore1674@pm.me>
2025-10-30 00:53:53 -0500

    update generate

--- src/lib/generate/index.ts
+++ src/lib/generate/index.ts
@@ -93,9 +93,11 @@ function generateCommand(
 
   if (!skip_changelog && existsSync(changelogPath)) {
     const changelogFile = readFileSync(changelogPath, { encoding: "utf8" });
-    const changelog = parseChangelog(changelogFile, releaseVersion);
-    changelogVersions = changelog.versions;
-    changelogLinks = changelog.links;
+    if (!changelogFile.trim()) {
+      const changelog = parseChangelog(changelogFile, releaseVersion);
+      changelogVersions = changelog.versions;
+      changelogLinks = changelog.links;
+    }
   }
 
   const files = readdirSync(`${process.cwd()}/${actionConfig.dir}`, {
